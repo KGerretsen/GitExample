9f2709570fa7355ec97b4b2a6b55bd5e5df85778
KGerretsen <33416856+KGerretsen@users.noreply.github.com>
2018-04-23 14:43:45 +0200

    Deployment ready

--- Server.js
+++ Server.js
@@ -7,6 +7,13 @@ app.all('*',  (req, res, next) => {
 
 const port = 8080;
 
+app.get('/hello', (req, res, next) =>{
+    res.status(200).json({
+        'msg' : 'Hello JEDI\'s'
+    });
+});
+
+
 app.listen(port, () =>{
     console.log('The magic happens at: localhost:' + port);
 });
